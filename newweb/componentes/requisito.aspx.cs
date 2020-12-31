23cd80e8da2dfa67c52a5b1b94a9403fa08c5fb7
owllxz <luxskkz@gmail.com>
2020-12-30 21:14:17 -0300

    Ejemplo Aspx

---
+++ newweb/componentes/requisito.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace newweb.componentes
+{
+    public partial class requisito : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Console.Write("HOla");
+        }
+    }
+}
