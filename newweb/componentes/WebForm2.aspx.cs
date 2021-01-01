396713941182afb9e0e6ff9d96c31f03e1f1527c
C0DAA <43933519+C0DAA@users.noreply.github.com>
2021-01-01 17:08:33 -0300

    requisito12 avance 1.1

--- newweb/componentes/WebForm2.aspx.cs
+++ newweb/componentes/WebForm2.aspx.cs
@@ -5,21 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-
-namespace newweb.componentes.requisito12
+namespace newweb.componentes
 {
-    
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class WebForm2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            Image1.Visible = true;
-        }
     }
-       
 }
--- newweb/componentes/requisito12/re12.aspx.designer.cs
+++ newweb/componentes/requisito12/re12.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace newweb.componentes.requisito12
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control Panel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+
+        /// <summary>
         /// Control Label1.
         /// </summary>
         /// <remarks>
@@ -49,5 +58,14 @@ namespace newweb.componentes.requisito12
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
+        /// Control Panel2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel2;
     }
 }
