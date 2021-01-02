b22e4b13a16ae19d3f1d5911a487ceb48943e125
owllxz <luxskkz@gmail.com>
2021-01-02 20:25:05 -0300

    Update 2

--- newweb/componentes/requisito1/requisito2.aspx.designer.cs
+++ newweb/componentes/requisito1/requisito2.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace newweb.componentes.requisito1
 
     public partial class requisito2
     {
-
-        /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
