e97ce1fc29b472eb5c6ccac71b678c9b6cda0b04
C0DAA <43933519+C0DAA@users.noreply.github.com>
2021-01-04 03:19:49 -0300

    requisito 12 listo

--- newweb/componentes/requisito12/re12.aspx.designer.cs
+++ newweb/componentes/requisito12/re12.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace newweb.componentes.requisito12
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control rolgrupo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList rolgrupo;
+
+        /// <summary>
         /// Control Button1.
         /// </summary>
         /// <remarks>
--- newweb/componentes/requisito5/re5.aspx.designer.cs
+++ newweb/componentes/requisito5/re5.aspx.designer.cs
@@ -76,14 +76,5 @@ namespace newweb.componentes.requisito5
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
-
-        /// <summary>
-        /// Control Button1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
